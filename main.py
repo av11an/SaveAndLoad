46e846b2540c1c27d1493a1d95387210b39aa09f
Evan Ellis <92198518+av11an@users.noreply.github.com>
2022-01-16 20:00:26 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pickle
 
+
 class Pixelart:
     def __init__(self, name, length, width, ar):
         self.name = name
@@ -22,18 +23,22 @@ class Pixelart:
 
     def __str__(self):
         return '\n{} has a length and width of {}, with an array code of {}.'.format(self.name, self.get_size(),
-                                                                                   self.get_array())
+                                                                                     self.get_array())
 
+
+# Save a new pixel art object function, saves it as a binary file.
+# If the filename already exist it just overwrites the file, might add a warning in the future
 def addNewPixelArt(name, length, width, array):
     newPixelArt = Pixelart(name, length, width, array)
     with open((name + '.pkl'), 'wb') as save_mcpa:
         pickle.dump(newPixelArt, save_mcpa)
 
-#main
+
+# main
 n = int(input("\nHow many Pixel Arts will you be adding?: "))
 
 for i in range(n):
-    name = input("\nName of Pixel Art: ")
+    name = input("\nName of Pixel Art [WARNING: If you name the pixel art the same name as one previously saved it will overwrite that pixel art!]: ")
     l = int(input("Length of Pixel Art: "))
     w = int(input("Width of Pixel Art: "))
     ar = input("Copy and Paste full 2D array: ")
@@ -42,7 +47,10 @@ for i in range(n):
 
 conVar = input("\nDo you want to load a Pixel Art? input [yes] or [no]: ")
 
-if (conVar == "yes"):
+# Load code, if the name exist it will print the __str__ function in the Pixelart class, if not it will say no saved data
+# If no files exist it will also say no saved data
+
+if conVar == "yes":
     nameOfArt = input("Which Pixel art? input [name]: ")
     try:
         with open((nameOfArt + '.pkl'), 'rb') as load_mcpa:
@@ -55,4 +63,3 @@ if (conVar == "yes"):
         print("\nNo saved Pixel Art found!")
 else:
     print("\nHave a good day.")
-
